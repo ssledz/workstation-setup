df98d4964031ca0d87ccc7f3c8b4793a2fa918a0
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-08-31 00:51:44 +0200

    adding scripts to move around

--- update-packages.sh
+++ update-packages.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-echo dir=$dir
-
-cd $dir
-
-./init-env.sh
-
-. ./env.sh
-
-ansible-playbook -i hosts --limit localhost -c local --ask-become-pass --tags "install-packages" site.yml
+./ansible-playbook-run.sh install-packages
